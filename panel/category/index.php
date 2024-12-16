74087f4cf60d0ee39f6b4526075f0b4ed377068d
Amin Ataei <amin_ataei@live.com>
2024-12-16 13:03:12 +0330

    add index for panel and category page with layout,assets

--- panel/category/index.php
+++ panel/category/index.php
@@ -1,19 +1,26 @@
+<?php
 
+require_once "../../functions/helpers.php";
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PHP panel</title>
-    <link rel="stylesheet" href="../../assets/css/bootstrap.min.css" media="all" type="text/css">
-    <link rel="stylesheet" href="../../assets/css/style.css" media="all" type="text/css">
+    <link rel="stylesheet" href="<?= asset('/assets/css/bootstrap.min.css'); ?> " media="all" type="text/css">
+    <link rel="stylesheet" href="<?= asset('/assets/css/style.css'); ?>" media="all" type="text/css">
 </head>
 <body>
 <section id="app">
+<?php require_once "../layouts/top-nav.html"; ?>
 
     <section class="container-fluid">
         <section class="row">
             <section class="col-md-2 p-0">
+            <?php require_once "../layouts/sidebar.html"; ?>
+
             </section>
             <section class="col-md-10 pt-3">
 
@@ -58,7 +65,7 @@
 
 </section>
 
-<script src="<?= asset('assets/js/jquery.min.js') ?>"></script>
-<script src="<?= asset('assets/js/bootstrap.min.js') ?>"></script>
+<script src="<?= asset('/assets/js/jquery.min.js'); ?>"></script>
+<script src="<?= asset('/assets/js/bootstrap.min.js'); ?>"></script>
 </body>
 </html>
--- panel/index.php
+++ panel/index.php
@@ -1,20 +1,28 @@
+<?php
 
+require_once "../functions/helpers.php";
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PHP panel</title>
-    <link rel="stylesheet" href="../assets/css/bootstrap.min.css" media="all" type="text/css">
-    <link rel="stylesheet" href="../assets/css/style.css" media="all" type="text/css">
+    <link rel="stylesheet" href="<?= asset('/assets/css/bootstrap.min.css'); ?> " media="all" type="text/css">
+    <link rel="stylesheet" href="<?= asset('/assets/css/style.css'); ?>" media="all" type="text/css">
 </head>
 <body>
 <section id="app">
+<?php require_once "layouts/top-nav.html"; ?>
 
 
     <section class="container-fluid">
         <section class="row">
+            
             <section class="col-md-2 p-0">
+            <?php require_once "layouts/sidebar.html"; ?>
+
             </section>
             <section class="col-md-10 pb-3">
 
@@ -36,7 +44,7 @@
 
 </section>
 
-<script src="../assets/js/jquery.min.js"></script>
-<script src="../assets/js/bootstrap.min.js"></script>
+<script src="<?= asset('/assets/js/jquery.min.js'); ?>"></script>
+<script src="<?= asset('/assets/js/bootstrap.min.js'); ?>"></script>
 </body>
 </html>
